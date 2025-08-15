6f29b3510f270875474f1082af5a406f9aca2111
rajadharsini2799@gmail.com <rajadharsini2799@gmail.com>
2025-08-15 12:10:21 +0530

    test

--- GMI_PROJECT/main.tf
+++ GMI_PROJECT/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone = lookup(var.availability_zones, count.index, null)
+  availability_zone = var.availability_zones[count.index]
   tags = { Name = "${var.project_name}-public-${count.index}" }
 }
 
@@ -25,7 +25,7 @@ resource "aws_subnet" "private" {
   count      = length(var.private_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
-  availability_zone = lookup(var.availability_zones, count.index, null)
+  availability_zone = var.availability_zones[count.index]
   tags = { Name = "${var.project_name}-private-${count.index}" }
 }
 
---
+++ GMI_PROJECT/terraform.tfvars
@@ -0,0 +1,17 @@
+# Project Details
+project_name           = "GMI_PROJECT"
+key_name               = "my-keypair"
+
+# VPC
+vpc_cidr               = "10.0.0.0/16"
+
+# Subnets
+public_subnet_cidrs    = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnet_cidrs   = ["10.0.101.0/24", "10.0.102.0/24"]
+
+# Availability Zones
+availability_zones     = ["ap-south-1a", "ap-south-1b"]
+
+# EC2 instance types
+instance_type_elk      = "t3.medium"
+instance_type_app      = "t3.medium"
